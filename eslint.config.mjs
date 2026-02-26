dd0a8fa7aa6e338342c35d93e9870b6ea299f1a6
Jim Geurts <jim@biacreations.com>
2026-02-26 11:45:56 -0600

    chore: Update npms, convert tests to vitest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,5 @@ import { config } from 'eslint-config-decent';
 
 export default config({
   tsconfigRootDir: import.meta.dirname,
-  enableJest: false,
-  enableVitest: false,
   enableTestingLibrary: false,
 });
